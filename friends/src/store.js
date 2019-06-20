8749bc694108dee1f046912a8bb517846e6796d9
Michael Harris <mhguitar1997@gmail.com>
2019-06-20 12:40:20 -0700

    Created redux store

--- friends/src/store.js
+++ friends/src/store.js
@@ -1,3 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+export default store
