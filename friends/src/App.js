a7b7178c42438c7027055c62263467fc0caf74b4
Michael Harris <mhguitar1997@gmail.com>
2019-06-20 13:12:12 -0700

    Fixed error: wrapper App component in Router

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
-import Login from './views/Login'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./views/Login";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Route path="/login" component={Login} />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
