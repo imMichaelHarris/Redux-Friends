de5fec55c33870e651d7051a8053816f8acba59e
Michael Harris <mhguitar1997@gmail.com>
2019-06-20 13:02:35 -0700

    Added a login route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import Login from './views/Login'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Route path="/login" component={Login} />
     </div>
   );
 }
