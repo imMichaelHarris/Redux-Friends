896333b11485ec886d99320b82973ebcc868240d
Michael Harris <mhguitar1997@gmail.com>
2019-06-20 13:09:15 -0700

    State and handleChanges function added in login component

--- friends/src/views/Login.js
+++ friends/src/views/Login.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 class Login extends React.Component {
-  state = {};
+  state = {
+      username: '',
+      password: '',
+  };
+
+  handleChanges = e => {
+      this.setState({
+          [e.target.name]: e.target.value
+      })
+  }
 
   render() {
     return (
       <div>
         <form>
-          <input type="text" name="username" value={this.state.username} onChange={handleChanges}/>
-          <input type="password" name="password" value={this.state.password} onChange={handleChanges} />
+          <input type="text" name="username" value={this.state.username} onChange={this.handleChanges}/>
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChanges} />
 
         </form>
       </div>
