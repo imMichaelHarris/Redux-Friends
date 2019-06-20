a0f884c49a2640bcb1f7b757058b8b8619c871a2
Michael Harris <mhguitar1997@gmail.com>
2019-06-20 13:15:12 -0700

    Login button and function added

--- friends/src/views/Login.js
+++ friends/src/views/Login.js
@@ -12,16 +12,21 @@ class Login extends React.Component {
       })
   }
 
+  login = e => {
+      e.preventDefault()
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.login}>
             <label>Username:</label>
           <input type="text" name="username" value={this.state.username} onChange={this.handleChanges}/>
           <label>Password:</label>
 
           <input type="password" name="password" value={this.state.password} onChange={this.handleChanges} />
 
+            <button>Login</button>
         </form>
       </div>
     );
