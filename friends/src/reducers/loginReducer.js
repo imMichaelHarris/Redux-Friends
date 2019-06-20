db9352e6f67136b3aa6c667efdc93d886f549175
Michael Harris <mhguitar1997@gmail.com>
2019-06-20 13:22:47 -0700

    Login reducer created

---
+++ friends/src/reducers/loginReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    error: '',
+    loggingIn: false
+}
+
+export default (state = initialState, action) => {
+    switch(action.type){
+        default: 
+        return state
+    }
+}
